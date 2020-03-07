6c2baf71c05ee646e7c78d336b4d9cca6eab8e3b
Derick Dsilva <derick.dsilva@axomic.com>
2020-03-07 18:06:29 +0000

    initial commit with basic working stuff of helo World

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import HelloWorld from "./Hello";
 
-function App() {
+/*class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Bingo App</h1>
+      </div>
+    );
+  }
+*/
+
+/*const App = () => {
+  return <div className="App">Hello World!</div>;
+};
+export default App;
+*/
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HelloWorld />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Hello.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Hello.css";
+
+const HelloWorld = () => {
+  return <div className="helloTitle">Hellow World!!</div>;
+};
+
+export default HelloWorld;

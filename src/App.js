e03d6a3c3a5a2a346a270802b098fc0db94ebc47
Derick Dsilva <derick.dsilva@axomic.com>
2020-03-07 18:23:03 +0000

    Moved Heloow world to sep compoennt and added class along with function for hello world

--- src/App.js
+++ src/App.js
@@ -2,28 +2,22 @@ import React, { Component } from "react";
 import "./App.css";
 import HelloWorld from "./Hello";
 
-/*class App extends Component {
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Bingo App</h1>
+      <div>
+        <HelloWorld />
       </div>
     );
   }
-*/
+}
 
 /*const App = () => {
-  return <div className="App">Hello World!</div>;
-};
-export default App;
-*/
-
-const App = () => {
   return (
     <div>
       <HelloWorld />
     </div>
   );
-};
+};*/
 
 export default App;
--- src/Hello.js
+++ src/Hello.js
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Hello.css";
+
+/*class HelloWorld extends Component {
+  render() {
+    return <div className="helloTitle">Hellow World!!</div>;
+  }
+}*/
 
 const HelloWorld = () => {
   return <div className="helloTitle">Hellow World!!</div>;

0f7cd03280bc4ead6aa55397faa907d8e6266905
Derick Dsilva <derick.dsilva@axomic.com>
2020-03-08 12:29:39 +0000

    Further componentification

--- src/HelloWorldList.js
+++ src/HelloWorldList.js
@@ -3,13 +3,19 @@ import "./HelloWorldList.css";
 import HelloWorld from "./HelloWorld";
 
 class HelloWorldList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { greetingTo: ["James", "Sally", "derd"] };
+  }
+
+  renderGreetings = () => {
+    return this.state.greetingTo.map(name => (
+      <HelloWorld key={name} name={name} />
+    ));
+  };
+
   render() {
-    return (
-      <div className="HelloWorldList">
-        <HelloWorld name="Jim" />
-        <HelloWorld name="Jim" />
-      </div>
-    );
+    return <div className="HelloWorldList">{this.renderGreetings()}</div>;
   }
 }
 export default HelloWorldList;

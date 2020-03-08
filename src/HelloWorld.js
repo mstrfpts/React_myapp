f98545c67de931256411a2f0a10573254809c8cf
Derick Dsilva <derick.dsilva@axomic.com>
2020-03-08 17:38:14 +0000

    Addded greeting add remove

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "./Hello.css";
-import "./HelloWorldList.css";
 
 class HelloWorld extends Component {
   constructor(props) {
@@ -15,13 +14,20 @@ class HelloWorld extends Component {
     this.setState({ greeting: "Bonjour" });
   };
 
+  removeGreeting = () => {
+    console.log("derd, in remove greeting");
+    this.props.removeGreeting(this.props.name);
+  };
+
   render() {
+    console.log("derd, hello", this.props);
     return (
       <div className="helloTitle">
         {this.state.greeting + " "}
         {this.props.name}!!
         <br />
         <button onClick={this.frenchify}>Frenchify!</button>
+        <button onClick={this.removeGreeting}>Remove</button>
       </div>
     );
   }
